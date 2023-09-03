7e60e885ecf5a0c3e74bd97e237c89677495b07c
VVS <vanina.v.stancheva@gmail.com>
2023-09-03 20:51:22 +0300

    create random sentance generator

---
+++ sentanceGenerator.js
@@ -0,0 +1,27 @@
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+function createRandomSentance() {
+    let names = ['Todor', 'Nikoleta', 'Vanina', 'Pesho', 'Desi'];
+    let places = ['Sofia', 'Plovdiv', 'Varna', 'Burgas'];
+    let verbs = ['eats', 'holds','sees', 'plays with', 'brings'];
+    let nouns = ['stones', 'cake', 'apple', 'laptop', 'bikes'];
+    let adverbs = ['slowly', 'diligently', 'warmly', 'sadly', 'rapidly'];
+    let details = ['near the river', 'at home', 'in the park'];
+    
+    function getRandomWord (array) {
+        return array[Math.floor(Math.random() * array.length)];
+    }
+    
+    const output = `${getRandomWord(names)} from ${getRandomWord(places)} ${getRandomWord(verbs)} ${getRandomWord(nouns)} ${getRandomWord(adverbs)} ${getRandomWord(details)}.`;
+
+    console.log(output);
+}
+
+console.log('Hello, this is your first random generated senctace.');
+createRandomSentance();
+readline.question('What is your age? ', (age) => {
+    console.log('Your age is: ' + age);
+});
